248e776062860da158fcae5efcebf7b6d3cbd075
Julike Manji <julianerrasti99@gmail.com>
2023-03-01 18:53:56 +0100

    Crud Alumno Done

---
+++ Alumno.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestor_Cuentas
+{
+    public class Alumno
+    {
+        // Campos de la clase Alumno
+        public int IdAlumno { get; set; }
+        public string Dni { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+
+        // Constructor completo de la clase Alumno
+        public Alumno(int idAlumno, string dni, string nombre, string apellido, DateTime fechaNacimiento)
+        {
+            IdAlumno = idAlumno;
+            Dni = dni;
+            Nombre = nombre;
+            Apellido = apellido;
+            FechaNacimiento = fechaNacimiento;
+        }
+
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySqlConnector;
 
 namespace Gestor_Cuentas
 {
@@ -20,11 +22,18 @@ namespace Gestor_Cuentas
     /// </summary>
     public partial class MainWindow : Window
     {
+        private AlumnoCrud alumnoCrud;
         public MainWindow()
         {
             InitializeComponent();
+            alumnoCrud = new AlumnoCrud();
         }
-
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Cargar los datos de la tabla Alumno en el DataGrid
+            tablaAlumno.ItemsSource = alumnoCrud.GetAllAlumnosAsDataTable().DefaultView;
+        }
+     
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
@@ -49,6 +58,80 @@ namespace Gestor_Cuentas
             gridDatosEmpresa.Visibility = Visibility.Hidden;
 
             gridTablaEmpresa.Visibility = Visibility.Visible;
+        }
+
+        private void limpiaCamposAlumno()
+        {
+            txfCodAlumno.Text = "";
+            txfDniAlumno.Text = "";
+            txfNombreAlumno.Text = "";
+            txfApellidosAlumno.Text = "";
+            dpFechaNacAlumno.SelectedDate = null;
+        }
+        private void btInsertAlumn_Click(object sender, RoutedEventArgs e)
+        {
+            int idAlumno = int.Parse(txfCodAlumno.Text);
+            string dni = txfDniAlumno.Text;
+            string nombre = txfNombreAlumno.Text;
+            string apellido = txfApellidosAlumno.Text;
+            DateTime fechaNacimiento = dpFechaNacAlumno.SelectedDate.Value;
+            Alumno alumno = new Alumno(idAlumno, dni, nombre, apellido, fechaNacimiento);
+            alumnoCrud.InsertAlumno(alumno);
+            MessageBox.Show("Alumno insertado correctamente");
+            tablaAlumno.ItemsSource = alumnoCrud.GetAllAlumnosAsDataTable().DefaultView;
+
+            limpiaCamposAlumno();
+
+            
+        }
+
+        private void btModiAlumn_Click(object sender, RoutedEventArgs e)
+        {
+            int idAlumno = int.Parse(txfCodAlumno.Text);
+            string dni = txfDniAlumno.Text;
+            string nombre = txfNombreAlumno.Text;
+            string apellido = txfApellidosAlumno.Text;
+            DateTime fechaNacimiento = dpFechaNacAlumno.SelectedDate.Value;
+            Alumno alumno = new Alumno(idAlumno, dni, nombre, apellido, fechaNacimiento);
+            alumnoCrud.UpdateAlumno(alumno);
+            MessageBox.Show("Alumno actualizado correctamente");
+            tablaAlumno.ItemsSource = alumnoCrud.GetAllAlumnosAsDataTable().DefaultView;
+            limpiaCamposAlumno();
+
+        }
+
+        private void btElimAlumn_Click(object sender, RoutedEventArgs e)
+        {
+            int idAlumno = int.Parse(txfCodAlumno.Text);
+            alumnoCrud.DeleteAlumno(idAlumno);
+            MessageBox.Show("Alumno eliminado correctamente");
+            tablaAlumno.ItemsSource = alumnoCrud.GetAllAlumnosAsDataTable().DefaultView;
+            limpiaCamposAlumno();
+
+        }
+
+        private void tablaAlumno_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
+        {
+            // Obtenemos la fila seleccionada del DataGrid
+            DataRowView rowView = tablaAlumno.SelectedItem as DataRowView;
+
+            // Comprobamos que se ha seleccionado alguna fila
+            if (rowView != null)
+            {
+                // Obtenemos los valores de las celdas de la fila seleccionada
+                int idAlumno = (int)rowView["idalumno"];
+                string dni = (string)rowView["dni"];
+                string nombre = (string)rowView["nombre"];
+                string apellido = (string)rowView["apellido"];
+                DateTime fechaNacimiento = (DateTime)rowView["fechaNacimiento"];
+
+                // Actualizamos los valores de los TextBox con los valores obtenidos
+                txfCodAlumno.Text = idAlumno.ToString();
+                txfDniAlumno.Text = dni;
+                txfNombreAlumno.Text = nombre;
+                txfApellidosAlumno.Text = apellido;
+                dpFechaNacAlumno.SelectedDate = fechaNacimiento;
+            }
         }
     }
 }
